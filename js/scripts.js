44c83022241e7c6e28e90038d9af038801632a56
Michał Świtoń <michal.switon@op.pl>
2018-01-02 00:20:04 +0100

    centered tree in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 function drawTree (heightTree) {
-    for (var i = 1; i <= heightTree; i++) {
-        var star = " ";
-        for (var j = 1; j <= i; j++) {
-            star += "*";
+    for (var i = 0; i <= heightTree; i++) {
+        var star = "";
+        for (var j = 0; j < (heightTree - i); j++) {
+            star += " ";
         }
+            for (var k = 0; k <= i*2; k++) {
+                star += "*";
+            }
+        
         console.log(star);
     }
 }
